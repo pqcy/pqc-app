c8e6091423668615bf0f21d0f42dbd2763caff46
cryptogus <60291830+cryptogus@users.noreply.github.com>
2022-12-03 01:26:30 +0900

    오류문 추가 및 while 추가

--- app/pcapdevice-test/pcapdevice-test.cpp
+++ app/pcapdevice-test/pcapdevice-test.cpp
@@ -76,27 +76,36 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    PcapDevice pcapdevice;
-    pcapdevice.open(argv[1]/*param.intfName_*/, BUFSIZ, 1, 1000);
-
-    struct pcap_pkthdr* header;
-    const u_char* packet;
-    pcapdevice.read(&header, &packet);
-
-    struct libnet_ethernet_hdr *p = (struct libnet_ethernet_hdr*)packet;
-    struct libnet_ipv4_hdr * p2 = (struct libnet_ipv4_hdr*)(packet+ 14);
-
-    printf("src mac: %02x:%02x:%02x:%02x:%02x:%02x\n",p->ether_shost[0],p->ether_shost[1],p->ether_shost[2],p->ether_shost[3],p->ether_shost[4],p->ether_shost[5]);
-    printf("des mac : %02x:%02x:%02x:%02x:%02x:%02x\n",p->ether_dhost[0],p->ether_dhost[1],p->ether_dhost[2],p->ether_dhost[3],p->ether_dhost[4],p->ether_dhost[5]);
-
-    if(ntohs(p->ether_type) == 0x0800){
-        printf("ip source address : %u.%u.%u.%u\n", p2->ip_src.s_addr &0xff, (p2->ip_src.s_addr >> 8 ) &0xff, (p2->ip_src.s_addr >> 16 ) &0xff, (p2->ip_src.s_addr>>24) &0xff); //s_addr 4바이트 변수 & little endian
-        printf("ip destination adress : %u.%u.%u.%u\n", p2->ip_dst.s_addr & 0xff, (p2->ip_dst.s_addr >> 8) & 0xff, (p2->ip_dst.s_addr >> 16)&0xff, (p2->ip_dst.s_addr >> 24)&0xff);
+    PcapDevice pcapDevice;
+    bool wh = pcapDevice.open(argv[1]/*param.intfName_*/, BUFSIZ, 1, 1000);
+    if(!wh){
+        printf("opne error\n");
+        return -1;
     }
-    else{
-        printf("None ip\n");
-    }
+    while(true){
 
-    pcapdevice.close();
+
+        struct pcap_pkthdr* header;
+        const u_char* packet;
+        int wh2 = pcapDevice.read(&header, &packet);
+        if(wh2 == -1){
+            printf("read error\n");
+            return -2;
+        }
+        struct libnet_ethernet_hdr *p = (struct libnet_ethernet_hdr*)packet;
+        struct libnet_ipv4_hdr * p2 = (struct libnet_ipv4_hdr*)(packet+ 14);
+
+        printf("src mac: %02x:%02x:%02x:%02x:%02x:%02x\n",p->ether_shost[0],p->ether_shost[1],p->ether_shost[2],p->ether_shost[3],p->ether_shost[4],p->ether_shost[5]);
+        printf("des mac : %02x:%02x:%02x:%02x:%02x:%02x\n",p->ether_dhost[0],p->ether_dhost[1],p->ether_dhost[2],p->ether_dhost[3],p->ether_dhost[4],p->ether_dhost[5]);
+
+        if(ntohs(p->ether_type) == 0x0800){
+            printf("ip source address : %u.%u.%u.%u\n", p2->ip_src.s_addr &0xff, (p2->ip_src.s_addr >> 8 ) &0xff, (p2->ip_src.s_addr >> 16 ) &0xff, (p2->ip_src.s_addr>>24) &0xff); //s_addr 4바이트 변수 & little endian
+            printf("ip destination adress : %u.%u.%u.%u\n", p2->ip_dst.s_addr & 0xff, (p2->ip_dst.s_addr >> 8) & 0xff, (p2->ip_dst.s_addr >> 16)&0xff, (p2->ip_dst.s_addr >> 24)&0xff);
+        }
+        else{
+            printf("None ip\n");
+        }
+    }
+    pcapDevice.close();
     return 0;
 }
