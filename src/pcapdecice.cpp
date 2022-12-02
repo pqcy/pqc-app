4f56454942feb5480b0932ea091b74a8bae2f678
cryptogus <60291830+cryptogus@users.noreply.github.com>
2022-12-03 00:02:08 +0900

    pcapdevice-hyun-latest

--- src/pcapdecice.cpp
+++ src/pcapdecice.cpp
@@ -19,13 +19,14 @@ bool PcapDevice::close() {
 }
 
 int PcapDevice::read(struct pcap_pkthdr **pkt_header,const u_char **pkt_data) {
-    int res = pcap_next_ex(pcap_, &pkt_header, &pkt_data);
+    int res = pcap_next_ex(pcap_, pkt_header, pkt_data);
     
     if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
         error_ = pcap_geterr(pcap_);
         return -1;
 	}
-    return pkt_header->caplen;
+    
+    return (*pkt_header)->caplen;
 }
 
 int PcapDevice::write(const u_char *buf, int size){
--- src/pcapdevice.h
+++ src/pcapdevice.h
@@ -6,10 +6,12 @@
 
 
 class PcapDevice{
-    string error_;
+
+public:
+    std::string error_;
      // pcap 그때그때 사용하게 초기화, pcap_t* pcap를 숨긴다, 즉 open,close,write,read에 pcap_t* pcap 인자로 줄 필요가 없다 공부 더 필요
     pcap_t* pcap_{nullptr};
-
+    
     bool open(const char *device, int snaplen, int promisc, int to_ms);
     bool close();
     int read(struct pcap_pkthdr **pkt_header,const u_char **pkt_data);
