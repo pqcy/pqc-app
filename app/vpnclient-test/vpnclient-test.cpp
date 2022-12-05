ff4e06d148b5e3271c67e61b9f686c44c51c211d
heahgo <imcastleempty@gmail.com>
2022-12-05 12:07:02 +0900

    Add vpn client code

--- app/vpnclient-test/vpnclient-test.cpp
+++ app/vpnclient-test/vpnclient-test.cpp
@@ -1,38 +1,49 @@
 #include "vpnclient.h"
 
-void usage()
-{
-    printf("syntax: vpnclient <dummy interface name> <real interface name> <server ip> <server port>\n");
-    printf("vpnclient dum0 wlan0 10.1.1.2 12345\n");
-}
-
-typedef struct
-{
+struct Param {
     char *dumDev_;
     char *realDev_;
-} Param;
+    Ip ip_;
+    int port_;
 
-Param param = {
-    .dumDev_ = NULL,
-    .realDev_ = NULL
-};
+    bool parse(int argc, char** argv) {
+        if (argc != 5) return false;
 
-bool parse(Param *param, int argc, char *argv[])
-{
-    if (argc != 5)
-    {
-        usage();
-        return false;
+        dumDev_ = argv[1];
+        realDev_ = argv[2];
+        ip_ = inet_addr(argv[3]);
+        port_ = std::stoi(argv[1]);
+        return true;
     }
-    param->dumDev_ = argv[1];
-    param->realDev_ = argv[2];
-    return true;
-}
+
+    static void usage()
+    {
+        printf("syntax: vpnclient <dummy interface name> <real interface name> <server ip> <server port>\n");
+        printf("vpnclient dum0 wlan0 10.1.1.2 12345\n");
+    }
+};
 
 int main(int argc, char *argv[])
 {
-    if (!parse(&param, argc, argv))
-        return -1;
-
     VpnClient vc;
+
+    Param param;
+    if (!param.parse(argc, argv)) {
+        Param::usage();
+        return -1;
+    }
+
+    if (!vc.tcpClient_.connect(param.ip_, param.port_)) {
+        printf("%s\n", vc.tcpClient_.error_.data());
+        return -1;
+    }
+//    struct pcap_pkthdr *header;
+//    const u_char *data;
+//    char *buf;
+//    int size = 4096;
+
+//    vc.captureAndWriteThread_(&VpnClient::captureAndWrite, header, data, size);
+//    vc.readAndSendMethread_(vc.readAndSendMe);
+
+    vc.tcpClient_.close();
 }
