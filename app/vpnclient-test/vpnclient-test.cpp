e150474a4f91d5f892ded3f63af7c0349483fd99
cryptogus <60291830+cryptogus@users.noreply.github.com>
2022-12-05 14:10:10 +0900

    segmantation fault

--- app/vpnclient-test/vpnclient-test.cpp
+++ app/vpnclient-test/vpnclient-test.cpp
@@ -1,67 +1,63 @@
 #include "vpnclient.h"
 #include <libnet.h>
 
-void usage()
-{
-    printf("syntax: vpnclient <dummy interface name> <real interface name> <server ip> <server port>\n");
-    printf("vpnclient dum0 wlan0 10.1.1.2 12345\n");
-}
-
-typedef struct
-{
+struct Param {
     char *dumDev_;
     char *realDev_;
-} Param;
+    Ip ip_;
+    int port_;
 
-Param param = {
-    .dumDev_ = NULL,
-    .realDev_ = NULL
-};
+    bool parse(int argc, char** argv) {
+        if (argc != 5) return false;
 
-bool parse(Param *param, int argc, char *argv[])
-{
-    if (argc != 5)
-    {
-        usage();
-        return false;
+        dumDev_ = argv[1];
+        realDev_ = argv[2];
+        ip_ = inet_addr(argv[3]);
+        port_ = std::stoi(argv[4]);
+        return true;
     }
-    param->dumDev_ = argv[1];
-    param->realDev_ = argv[2];
-    return true;
-}
+
+    static void usage()
+    {
+        printf("syntax: vpnclient <dummy interface name> <real interface name> <server ip> <server port>\n");
+        printf("vpnclient dum0 wlan0 10.1.1.2 12345\n");
+    }
+};
 
 int main(int argc, char *argv[])
 {
-    if (!parse(&param, argc, argv))
-        return -1;
+    Param param;
 
+    if (!param.parse(argc, argv)){
+        Param::usage();
+        return -1;
+    }
     VpnClient vc;
-    vc.open(param.dumDev_, (Ip)argv[3], std::stoi(argv[4]));
+
+    vc.open(param.dumDev_, param.ip_, param.port_);
 
     struct pcap_pkthdr* header;
     const u_char* packet;
 
-    char buf[1000];
+    //char buf[1000] = {0,};
 
     vc.captureAndWrite(header, packet, 1000);
 
-    struct libnet_ethernet_hdr *p = (struct libnet_ethernet_hdr*)packet;
-    struct libnet_ipv4_hdr * p2 = (struct libnet_ipv4_hdr*)(packet+ 14);
+    while(true){
+        struct libnet_ethernet_hdr *p = (struct libnet_ethernet_hdr*)packet;
+        struct libnet_ipv4_hdr * p2 = (struct libnet_ipv4_hdr*)(packet+ 14);
 
-    printf("src mac: %02x:%02x:%02x:%02x:%02x:%02x\n",p->ether_shost[0],p->ether_shost[1],p->ether_shost[2],p->ether_shost[3],p->ether_shost[4],p->ether_shost[5]);
-    printf("des mac : %02x:%02x:%02x:%02x:%02x:%02x\n",p->ether_dhost[0],p->ether_dhost[1],p->ether_dhost[2],p->ether_dhost[3],p->ether_dhost[4],p->ether_dhost[5]);
+        printf("src mac: %02x:%02x:%02x:%02x:%02x:%02x\n",p->ether_shost[0],p->ether_shost[1],p->ether_shost[2],p->ether_shost[3],p->ether_shost[4],p->ether_shost[5]);
+        printf("des mac : %02x:%02x:%02x:%02x:%02x:%02x\n",p->ether_dhost[0],p->ether_dhost[1],p->ether_dhost[2],p->ether_dhost[3],p->ether_dhost[4],p->ether_dhost[5]);
 
-    if(ntohs(p->ether_type) == 0x0800){
-        printf("ip source address : %u.%u.%u.%u\n", p2->ip_src.s_addr &0xff, (p2->ip_src.s_addr >> 8 ) &0xff, (p2->ip_src.s_addr >> 16 ) &0xff, (p2->ip_src.s_addr>>24) &0xff); //s_addr 4바이트 변수 & little endian
-        printf("ip destination adress : %u.%u.%u.%u\n", p2->ip_dst.s_addr & 0xff, (p2->ip_dst.s_addr >> 8) & 0xff, (p2->ip_dst.s_addr >> 16)&0xff, (p2->ip_dst.s_addr >> 24)&0xff);
+        if(ntohs(p->ether_type) == 0x0800){
+            printf("ip source address : %u.%u.%u.%u\n", p2->ip_src.s_addr &0xff, (p2->ip_src.s_addr >> 8 ) &0xff, (p2->ip_src.s_addr >> 16 ) &0xff, (p2->ip_src.s_addr>>24) &0xff); //s_addr 4바이트 변수 & little endian
+            printf("ip destination adress : %u.%u.%u.%u\n", p2->ip_dst.s_addr & 0xff, (p2->ip_dst.s_addr >> 8) & 0xff, (p2->ip_dst.s_addr >> 16)&0xff, (p2->ip_dst.s_addr >> 24)&0xff);
+        }
+        else{
+            printf("None ip\n");
+        }
     }
-    else{
-        printf("None ip\n");
-    }
-
-    memset(buf, 0, 1000);
-    vc.readAndSendMe(buf, 1000);
-
 
     vc.close();
 }
